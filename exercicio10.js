e87b94aab927c73e313558950b6b5d4d17f233d8
Macauly Fragoso <macaulyaparecido@gmail.com>
2025-05-19 01:27:27 -0300

    Exercício 10: Exibir um número inteiro 10 vezes

---
+++ exercicio10.js
@@ -0,0 +1,8 @@
+function exibirNumeroDezVezes(numero) {
+    for (let i = 0; i < 10; i++) {
+        console.log(numero);
+    }
+}
+
+let numero = parseInt(prompt("Digite um número inteiro:"));
+exibirNumeroDezVezes(numero);
